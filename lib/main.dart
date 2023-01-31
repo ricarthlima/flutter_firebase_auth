9167abd3f648ff33f26a12a96f548581b5ccbbfd
ricarthlima <rrsl@cin.ufpe.br>
2023-01-31 10:23:48 -0300

    projeto inicial (tela de login/cadastro)
    projeto inicial (tela login/cadastro)

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_firebase_firestore_second/_core/my_colors.dart';
+import 'package:flutter_firebase_firestore_second/authenticator/screens/auth_screen.dart';
 import 'firebase_options.dart';
 import 'firestore/presentation/home_screen.dart';
 
@@ -49,7 +50,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const HomeScreen(),
+      home: const AuthScreen(),
     );
   }
 }
