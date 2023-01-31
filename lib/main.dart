e69087ef90d355dffce4751f2bb8301fc7945dae
ricarthlima <rrsl@cin.ufpe.br>
2023-01-31 16:44:29 -0300

    v4.1 - roteando tela inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_firebase_firestore_second/_core/my_colors.dart';
 import 'package:flutter_firebase_firestore_second/authenticator/screens/auth_screen.dart';
+import 'package:flutter_firebase_firestore_second/firestore/presentation/home_screen.dart';
 import 'firebase_options.dart';
-import 'firestore/presentation/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -50,7 +51,29 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const AuthScreen(),
+      home: const RoteadorTelas(),
+    );
+  }
+}
+
+class RoteadorTelas extends StatelessWidget {
+  const RoteadorTelas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.userChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else {
+          if (snapshot.hasData) {
+            return const HomeScreen();
+          } else {
+            return const AuthScreen();
+          }
+        }
+      },
     );
   }
 }
